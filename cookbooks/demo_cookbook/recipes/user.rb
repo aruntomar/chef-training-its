8ff714bf019fbd124127ed998280abaf1d2ee434
Arun Tomar <arun@solutionenterprises.co.in>
2015-10-30 15:02:50 +0530

    data bag demo

--- cookbooks/demo_cookbook/recipes/user.rb
+++ cookbooks/demo_cookbook/recipes/user.rb
@@ -4,29 +4,33 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-group 'arun' do
-  action :create
-end
+user_result = search(:users,"id:*")
 
-user 'arun' do
-  comment 'chef user'
-  uid '10001'
-  gid 'arun'
-  shell '/bin/bash'
-  manage_home true
-end
+user_result.each do |u|
+  group u['id'] do
+    action :create
+  end
 
-directory '/home/arun/.ssh' do
-  owner 'arun'
-  group 'arun'
-  mode '0744'
-  recursive true
-  action :create
-end
+  user u['id'] do
+    comment 'chef user'
+    gid u['id']
+    shell '/bin/bash'
+    manage_home true
+  end
 
-cookbook_file '/home/arun/.ssh/authorized_keys' do
-  source 'arun_authorized_keys'
-  owner 'arun'
-  group 'arun'
-  mode '0644'
+  directory "/home/#{u['id']}/.ssh" do
+    owner u['id']
+    group u['id']
+    mode '0744'
+    recursive true
+    action :create
+  end
+
+  cookbook_file "/home/#{u['id']}/.ssh/authorized_keys" do
+    source 'arun_authorized_keys'
+    owner u['id']
+    group u['id']
+    mode '0644'
+  end
+
 end
--- cookbooks/windows_software/recipes/default.rb
+++ cookbooks/windows_software/recipes/default.rb
@@ -5,12 +5,12 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 windows_package 'PuTTY version 0.60' do
-  source 'http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.60-installer.exe'
+  source 'http://the.earth.li/~sgtatham/putty/latest/x86/putty.exe'
   installer_type :inno
   action :install
 end
 
 windows_package '7-Zip 9.20 (x64 edition)' do
-  source 'http://downloads.sourceforge.net/sevenzip/7z920-x64.msi'
+  source 'http://www.7-zip.org/a/7z920-x64.msi'
   action :install
 end
